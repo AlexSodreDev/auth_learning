43d41a9da8f29f23349480dcf9f49866635458ea
AlexSodreDev <alexandre.cortines@gmail.com>
2021-05-18 19:23:56 -0300

    remove ts-node, nodemon

---
+++ src/routes.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+
+const router = Router();
+
+export default router;
+
+// internet caindo muito, problemas com o ts-node, versão do vídeo 2.0.4 watching ts,json... versão da minha api de teste 2.0.7 watching js, mjs, json... Video parado em 3:34
