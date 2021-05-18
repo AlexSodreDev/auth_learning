003e9e2083f401e7979fd3284c2a9241a2f9fe10
AlexSodreDev <alexandre.cortines@gmail.com>
2021-05-18 20:27:12 -0300

    add typeorm, reflect-metadata

--- src/routes.ts
+++ src/routes.ts
@@ -3,5 +3,3 @@ import { Router } from 'express';
 const router = Router();
 
 export default router;
-
-// internet caindo muito, problemas com o ts-node, versão do vídeo 2.0.4 watching ts,json... versão da minha api de teste 2.0.7 watching js, mjs, json... Video parado em 3:34
